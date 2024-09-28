f01f629651d7e87aab3ed326d9ab86be3c841ce0
Daniel Krastev <danielkrastev01@gmail.com>
2024-09-28 20:53:29 +0300

    Add files via upload

--- Advanced-JS-SEPTEMBER-2024/Exercise-DOM-Manipulations-and-Events/01. Sections/script.js
+++ Advanced-JS-SEPTEMBER-2024/Exercise-DOM-Manipulations-and-Events/01. Sections/script.js
@@ -1,4 +1,5 @@
 function create(words) {
+   /*
    const contentEl = document.querySelector('#content');
 
    words.forEach((word) => {
@@ -19,5 +20,30 @@ function create(words) {
       contentEl.appendChild(divEl);
 
    })
-   
+   */
+
+
+   const container = document.getElementById('content');
+
+   for (const word of words) {
+      const div = createSelection(word);
+      container.appendChild(div);
+   }
+
+   function createSelection(content){
+      const div = document.createElement('div');
+      const para = document.createElement('p');
+      para.style.display = 'none';
+      para.textContent = content;
+      div.appendChild(para);
+
+      div.addEventListener('click', () => reveal(para));
+
+      return div;
+
+   }
+
+   function reveal(para){
+      para.style.display = '';      
+   }
 }
--- Advanced-JS-SEPTEMBER-2024/Exercise-DOM-Manipulations-and-Events/06. Furniture/solution.js
+++ Advanced-JS-SEPTEMBER-2024/Exercise-DOM-Manipulations-and-Events/06. Furniture/solution.js
@@ -1,12 +1,12 @@
-function solve() {
-  /*
+/*function solve() {
+  
   tr
   td image
   td name
   td price
   td decaration
   td checkbox
-  */
+  
 
   const btnElGen = document.querySelector('button');
 
@@ -76,4 +76,70 @@ function solve() {
     outputEl.value = output;
     
   })
+}*/
+
+function solve(){
+  const td = (content) => e('td', {}, content);
+  const img = (src) => e('img', { src });
+  const check = () => e('input', {type: 'checkbox'});
+
+  const [input, output] = document.querySelectorAll('textarea');
+  const [generateBtn, buyBtn] = document.querySelectorAll('button');
+  const table = document.querySelector('tbody');
+
+  generateBtn.addEventListener('click', parseInput);
+  buyBtn.addEventListener('click', summary);
+
+  let data = [];
+
+  function parseInput() {
+    const json = input.value;
+    data = JSON.parse(json);
+
+    for (const entry of data) {
+      table.appendChild(createRow(entry));
+    }
+  }
+
+  function createRow(entry){
+    
+    const row = document.createElement('tr');
+    row.appendChild(td(img(entry.img)));
+    row.appendChild(td(document.createTextNode(entry.name)));
+    row.appendChild(td(document.createTextNode(entry.price)));
+    row.appendChild(td(document.createTextNode(entry.decFactor)));
+    
+    const checkbox = check();
+    row.appendChild(td(checkbox));
+
+    entry.checked = () => checkbox.checked;
+
+    return row;
+  }
+  
+  function summary(){
+    const selected = data.filter(e => e.checked());
+    
+    const result = [
+      `Bought furniture: ${data.map(i => i.name).join(', ')}`,
+      `Total price: ${Number(data.reduce((acc, el) => {return Number(acc) + Number(el.price)}, 0)).toFixed(2)}`,
+      `Average decoration factor: ${data.reduce((acc, el) => {return Number(acc) + Number(el.decFactor)} , 0) / data.length}`
+    ]
+
+    output.value = result.join('\n')
+    
+  }
+
+  function e(type, attr, content){
+    const result = document.createElement(type);
+
+    Object.assign(result, attr);
+
+    if(content){
+      result.appendChild(content);
+    }
+
+    return result;
+  }
+  
 }
